56a5500bce1ca80b61e63810b723f50dc1506a35
Algis <you@example.com>
2024-05-08 15:50:44 +0200

    merged

---
+++ js/countNumbers.js
@@ -0,0 +1,33 @@
+function startCounting() {
+    const targets = [15514, 1201, 2150, 3800]; // Final values for each counter
+    const duration = 300000; // Duration of the counting animation in milliseconds
+    
+    const counters = document.querySelectorAll('.stats h1');
+
+    counters.forEach((counter, index) => {
+        const increment = targets[index] / (duration / 1000); // Calculate increment per millisecond
+        let currentCount = 0;
+
+        const updateCount = () => {
+            currentCount += increment;
+            if (currentCount >= targets[index]) {
+                currentCount = targets[index];
+                clearInterval(interval);
+            }
+            counter.textContent = Math.floor(currentCount).toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+        };
+
+        const interval = setInterval(updateCount, 1);
+        updateCount();
+    });
+}
+
+// Call the function when the element with class 'stats' comes into view
+const statsSection = document.querySelector('.stats');
+const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+        startCounting();
+        observer.unobserve(statsSection);
+    }
+});
+observer.observe(statsSection);
